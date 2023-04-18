1ea01ebe1627f1d5e25ac273d7a45f4a6ba2ad8e
Kyle Rego <regoky@outlook.com>
2023-04-18 09:08:00 -0400

    Clean up specs not being used yet

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,3 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-
-# rubocop:disable RSpec/PendingWithoutReason
-RSpec.describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-# rubocop:enable RSpec/PendingWithoutReason
