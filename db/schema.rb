531caceb091ebf71428a0d91df08fd9e84d2e1af
Kyle Rego <regoky@outlook.com>
2023-04-30 10:21:53 -0400

    Add unique indexes to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_29_151908) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_30_141813) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_29_151908) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
