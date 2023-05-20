50bf9788fad2677db8ef19752b7d045bba9d0f19
Kyle Rego <regoky@outlook.com>
2023-05-20 12:26:44 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,16 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 book = Book.create!(title: "System book")
-Article.create!(title: "Hello World", system: true, book:)
+Article.create!(title: "Homepage system article", system: true, book:)
+
+user = User.create!(username: "test user", email: "test@example.com", password: "1234asdf!!!!")
+user.books << book
+
+5.times do |i|
+  normal_article = book.articles.create!(title: "Normal article #{i}")
+  10.times do |j|
+    front = "front of article #{i} note #{j}"
+    back = "back of article #{i} note #{j}"
+    normal_article.basic_notes.create!(front:, back:, ordinal_position: j)
+  end
+end
