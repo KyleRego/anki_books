cde8a636f061bbe99e5f05c3aed603eef13a9848
Kyle Rego <regoky@outlook.com>
2023-06-25 11:18:21 -0400

    Refactor top nav

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -8,14 +8,6 @@ module ArticlesHelper
     request.path.end_with?("study_cards")
   end
 
-  def show_link_to_study_cards_in_top_nav?(article)
-    return false unless request.path.match?(ARTICLE_PATH_REGEX) || request.path == "/"
-
-    return false if article.notes_count.zero?
-
-    true
-  end
-
   def back_to_article_path(article)
     return root_path if article.system
 
