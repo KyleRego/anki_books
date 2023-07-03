96d7850175f39c1597838cfa94946d89f2f44499
Kyle Rego <regoky@outlook.com>
2023-07-03 18:30:45 -0400

    Reorganize basic note step definitions

--- features/step_definitions/article_reordering_steps.rb
+++ features/step_definitions/article_reordering_steps.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
-When "I drag the note at position {string} to the dropzone at position {string}" do |position1, position2|
-  notes = page.all "[id^=#{BasicNote::TurboFrameable::TURBO_BASIC_NOTE_ID_PREFIX}]"
-  dragged_note = notes[position1.to_i].find("[draggable=\"true\"]")
-  target_dropzone = notes[position2.to_i].find(".note-droppable-area")
-  dragged_note.drag_to(target_dropzone, delay: 0.5, html5: true)
-  sleep 0.5
-end
-
-Then "the front of the note at position {string} should be {string}" do |position, front|
-  notes = page.all "[id^=#{BasicNote::TurboFrameable::TURBO_BASIC_NOTE_ID_PREFIX}]"
-  note_at_specified_ordinal_position = notes[position.to_i].find("[draggable=\"true\"]")
-  expect(note_at_specified_ordinal_position.text).to include front
-end
-
 Then "I should not see two adjacent New note links" do
   expect(page.text).not_to include "New note\nNew note"
 end
--- features/step_definitions/basic_notes_steps.rb
+++ features/step_definitions/basic_notes_steps.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-# TODO: Method to wrap the constant used below
+def turbo_basic_note_id_selector
+  "[id^=#{BasicNote::TurboFrameable::TURBO_BASIC_NOTE_ID_PREFIX}]"
+end
 
 When "I click {string} on the basic note" do |string|
-  within("[id^=#{BasicNote::TurboFrameable::TURBO_BASIC_NOTE_ID_PREFIX}]") do
+  within(turbo_basic_note_id_selector) do
     click_link string
   end
 end
 
 When "I fill in the basic note edit form with the following data:" do |table|
-  within("[id^=#{BasicNote::TurboFrameable::TURBO_BASIC_NOTE_ID_PREFIX}]") do
+  within(turbo_basic_note_id_selector) do
     table.hashes.each do |row|
       fill_in row["Field"], with: row["Value"]
     end
@@ -17,7 +19,7 @@ When "I fill in the basic note edit form with the following data:" do |table|
 end
 
 Then "I should see {string} in a basic note" do |text|
-  page.assert_selector("[id^=#{BasicNote::TurboFrameable::TURBO_BASIC_NOTE_ID_PREFIX}]", text:)
+  page.assert_selector(turbo_basic_note_id_selector, text:)
 end
 
 Then "the article's basic note with front {string} should be at ordinal position {string}" do |front, position|
@@ -26,6 +28,20 @@ end
 
 Then(/I should see the (\d+)(?:st|nd|rd|th)? basic note of the article has front "(.*?)"$/) do |position, text|
   index = position.to_i - 1
-  basic_notes = all("[id^=#{BasicNote::TurboFrameable::TURBO_BASIC_NOTE_ID_PREFIX}]")
+  basic_notes = all(turbo_basic_note_id_selector)
   expect(basic_notes[index].text).to include(text)
+end
+
+When "I drag the note at position {string} to the dropzone at position {string}" do |position1, position2|
+  notes = page.all turbo_basic_note_id_selector
+  dragged_note = notes[position1.to_i].find("[draggable=\"true\"]")
+  target_dropzone = notes[position2.to_i].find(".note-droppable-area")
+  dragged_note.drag_to(target_dropzone, delay: 0.5, html5: true)
+  sleep 0.5
+end
+
+Then "the front of the note at position {string} should be {string}" do |position, front|
+  notes = page.all turbo_basic_note_id_selector
+  note_at_specified_ordinal_position = notes[position.to_i].find("[draggable=\"true\"]")
+  expect(note_at_specified_ordinal_position.text).to include front
 end
