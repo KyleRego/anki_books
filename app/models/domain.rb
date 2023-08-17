5e1706de87068ce18b23d382c28b744b587e5dd4
Kyle Rego <regoky@outlook.com>
2023-08-16 22:59:04 -0400

    Update model annotations

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -4,14 +4,16 @@
 #
 # Table name: domains
 #
-#  id         :uuid             not null, primary key
-#  title      :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :uuid             not null
+#  id               :uuid             not null, primary key
+#  title            :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  user_id          :uuid             not null
+#  parent_domain_id :uuid
 #
 # Foreign Keys
 #
+#  fk_rails_...  (parent_domain_id => domains.id)
 #  fk_rails_...  (user_id => users.id)
 #
 class Domain < ApplicationRecord
