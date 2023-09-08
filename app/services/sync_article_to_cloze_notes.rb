0a6e830e64bf16e6977d15a443181c61b5c54838
Kyle Rego <regoky@outlook.com>
2023-09-08 07:43:42 -0400

    More progress on cloze notes

--- app/services/sync_article_to_cloze_notes.rb
+++ app/services/sync_article_to_cloze_notes.rb
@@ -4,6 +4,10 @@
 
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/MethodLength
+
 ##
 # Syncs the cloze sentences of the article with its cloze notes
 class SyncArticleToClozeNotes
@@ -11,24 +15,98 @@ class SyncArticleToClozeNotes
     new(article:, user:).perform
   end
 
-  attr_reader :article, :concept_names
+  attr_reader :article, :concepts
+
+  attr_accessor :sentence_concepts_matches, :cloze_note_sentence_matches
 
   def initialize(article:, user:)
     @article = article
-    @concept_names = user.concepts.pluck(:name)
+    @concepts = user.concepts
+    @sentence_concepts_matches = []
+    @cloze_note_sentence_matches = []
+  end
+
+  ##
+  # A mapping between an article cloze sentence and concepts
+  class SentenceConceptsMatch
+    def initialize(sentence:)
+      @sentence = sentence
+      @concepts = []
+    end
+
+    attr_reader :sentence, :concepts
+
+    def <<(concept)
+      @concepts << concept
+    end
+
+    private
+
+    def concept_names
+      concepts.map(&:name).join(", ")
+    end
+
+    def inspect
+      "\"Match sentence: #{sentence} concepts: #{concept_names}\""
+    end
+  end
+
+  ##
+  # A mapping between an article cloze sentence and one of the article's cloze notes
+  class ClozeNoteSentenceMatch
+    def initialize(sentence:, cloze_note:, distance:)
+      @sentence = sentence
+      @cloze_note = cloze_note
+      @distance = distance
+    end
+
+    attr_reader :distance
+
+    private
+
+    attr_reader :cloze_note, :sentence
+
+    def inspect
+      "\"Match cloze: #{sentence} article: #{cloze_note.sentence} distance: #{distance}\""
+    end
   end
 
   def perform
-    concept_names.each do |concept_name|
-      sentences = article.cloze_sentences(concept_name:)
+    concepts.each do |concept|
+      concept_name = concept.name
+      article_cloze_sentences = article.cloze_sentences(concept_name:)
+      article_cloze_sentences.each do |article_cloze_sentence|
+        match = sentence_concepts_matches.find { |sc_match| sc_match.sentence == article_cloze_sentence }
+        match ||= SentenceConceptsMatch.new(sentence: article_cloze_sentence)
 
-      # rubocop:disable Lint/UselessAssignment
-      sentences.each do |sentence|
-        levenshtein_ordered_cloze_notes = ClozeNote
-                                          .select("*, levenshtein(sentence, '#{sentence}'::text) AS distance")
-                                          .order("distance ASC")
-        # rubocop:enable Lint/UselessAssignment
+        match << concept
+        sentence_concepts_matches << match
       end
     end
+
+    sentence_concepts_matches.each do |sentence_concepts_match|
+      sentence = sentence_concepts_match.sentence
+
+      levenshtein_ordered_cloze_notes = ClozeNote
+                                        .select("*, levenshtein(sentence, '#{sentence}'::text) AS distance")
+                                        .order("distance ASC")
+
+      levenshtein_ordered_cloze_notes.each do |cloze_note_with_distance|
+        cloze_note_sentence_matches << ClozeNoteSentenceMatch.new(sentence:,
+                                                                  cloze_note: cloze_note_with_distance,
+                                                                  distance: cloze_note_with_distance.distance)
+      end
+    end
+
+    # rubocop:disable Lint/UselessAssignment
+    sorted_cloze_note_sentence_matches = cloze_note_sentence_matches.sort_by(&:distance)
+    cloze_note_sentence_matches = sorted_cloze_note_sentence_matches
+    # rubocop:enable Lint/UselessAssignment
+
+    # p sentence_concepts_matches
+    # p cloze_note_sentence_matches
   end
 end
+# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/MethodLength
--- spec/services/sync_article_to_cloze_notes_perform_spec.rb
+++ spec/services/sync_article_to_cloze_notes_perform_spec.rb
@@ -18,16 +18,16 @@ RSpec.describe SyncArticleToClozeNotes, "#perform" do
     end
   end
 
-  context "when article has content but user has no concepts" do
-    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests. UDP." }
+  context "when user has no concepts" do
+    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests." }
 
     it "does not change the number of cloze notes" do
       expect { sync_article_to_cloze_notes }.not_to change(ClozeNote, :count)
     end
   end
 
-  context "when article has content and user has one concept which does not match" do
-    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests. UDP." }
+  context "when user has one concept which does not match" do
+    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests." }
 
     before { create(:concept, user:, name: "Rainbow") }
 
@@ -36,20 +36,20 @@ RSpec.describe SyncArticleToClozeNotes, "#perform" do
     end
   end
 
-  context "when article has content, user has one matching concept, and article has no cloze notes" do
-    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests. UDP." }
+  context "when user has one matching concept, and article has no cloze notes" do
+    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests." }
 
     before { create(:concept, user:, name: "UDP") }
 
     it "creates a cloze note for the concept" do
-      pending "creating cloze notes"
+      pending "implementation"
       expect { sync_article_to_cloze_notes }.to change(ClozeNote, :count).by(1)
       expect(user.cloze_notes.first.sentence).to eq "UDP is a protocol."
     end
   end
 
-  context "when article has content, user has one matching concept, and cloze note is same as sentence" do
-    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests. UDP." }
+  context "when user has one matching concept, and cloze note is same as sentence" do
+    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests." }
 
     before do
       concept = create(:concept, user:, name: "UDP")
@@ -59,6 +59,94 @@ RSpec.describe SyncArticleToClozeNotes, "#perform" do
     it "does not update the cloze note" do
       expect { sync_article_to_cloze_notes }.not_to change(ClozeNote, :count)
       expect(article.cloze_notes.first.sentence).to eq "UDP is a protocol."
+    end
+  end
+
+  context "when user has one matching concept, a same cloze note, and a stale cloze note" do
+    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests." }
+
+    before do
+      concept = create(:concept, user:, name: "UDP")
+      create(:cloze_note, sentence: "UDP is a protocol.", concepts: [concept], article:)
+      create(:cloze_note, sentence: "Kitty cats.", concepts: [concept], article:)
+    end
+
+    it "does not update the cloze note that's the same and deletes the stale one" do
+      pending "implementation"
+      expect { sync_article_to_cloze_notes }.to change(ClozeNote, :count).by(-1)
+      expect(article.cloze_notes.first.sentence).to eq "UDP is a protocol."
+    end
+  end
+
+  context "when user has two matching concepts, a same cloze note, and a stale cloze note" do
+    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests." }
+
+    before do
+      create(:concept, user:, name: "UDP")
+      concept = create(:concept, user:, name: "Ethernet")
+      create(:cloze_note, sentence: "Kitty cats.", concepts: [concept], article:)
+    end
+
+    it "creates two cloze notes and deletes the stale note" do
+      pending "implementation"
+      expect { sync_article_to_cloze_notes }.to change(ClozeNote, :count).by(2)
+      expect(article.cloze_notes.pluck(:sentence).sort).to eq ["Ethernet is in the link layer", "UDP is a protocol."]
+    end
+  end
+
+  context "when user has two concepts which match the same sentence and no cloze notes" do
+    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests." }
+
+    before do
+      create(:concept, user:, name: "UDP")
+      create(:concept, user:, name: "link layer")
+    end
+
+    it "creates one cloze note for the two concepts" do
+      pending "implementation"
+      expect { sync_article_to_cloze_notes }.to change(ClozeNote, :count).by(2)
+      expect(article.cloze_notes.pluck(:sentence).sort).to eq ["Ethernet is in the link layer."]
+    end
+  end
+
+  context "when user has two concepts which match the same sentence and an old cloze note for that sentence" do
+    let(:content) { "TCP is a protocol. UDP is a protocol. Ethernet is in the link layer. Tests." }
+
+    before do
+      concept_one = create(:concept, user:, name: "Ethernet")
+      concept_two = create(:concept, user:, name: "link layer")
+      create(:cloze_note, sentence: "Ethernet is in the blimpy link layer.",
+                          concepts: [concept_one, concept_two],
+                          article:)
+    end
+
+    it "updates the cloze note for the two concepts" do
+      pending "implementation"
+      expect { sync_article_to_cloze_notes }.not_to change(ClozeNote, :count)
+      expect(article.cloze_notes.pick(:sentence)).to eq "Ethernet is in the link layer."
+    end
+  end
+
+  context "when article has many sentences and user has many concepts" do
+    let(:content) { "TCP. UDP. Ethernet. Link layer. Transport layer. Internet. IP. Packet. Segment. Datagram." }
+
+    before do
+      concept_one = create(:concept, user:, name: "TCP")
+      concept_two = create(:concept, user:, name: "UDP")
+      concept_three = create(:concept, user:, name: "Link layer")
+      concept_four = create(:concept, user:, name: "Internet")
+      create(:cloze_note, sentence: "Ethernet is in the blimpy Link layer.",
+                          concepts: [concept_three],
+                          article:)
+      create(:cloze_note, sentence: "TCP and UDP.", concepts: [concept_one, concept_two], article:)
+      create(:cloze_note, sentence: "Internet and web.", concepts: [concept_four], article:)
+      create(:cloze_note, sentence: "Transport layer.", article:)
+    end
+
+    it "syncs the article content sentence to the article cloze notes" do
+      pending "implementation"
+      expect { sync_article_to_cloze_notes }.not_to change(ClozeNote, :count)
+      expect(article.cloze_notes.pick(:sentence)).to eq ""
     end
   end
 end
