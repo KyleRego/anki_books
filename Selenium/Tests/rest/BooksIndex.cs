5d52ad5a59be6ab058c93b15c76d7f2b73d483b0
Kyle Rego <regoky@outlook.com>
2023-11-18 22:47:36 -0500

    Different screen size testing books#index

--- Selenium/Tests/rest/BooksIndex.cs
+++ Selenium/Tests/rest/BooksIndex.cs
@@ -10,28 +10,55 @@ using Tests.Extensions;
 [TestFixture]
 public class BooksIndex : AppTests
 {
-  [Test]
-  public void Test()
-  {
-    driver.Navigate().GoToUrl("http://localhost:3000/");
-    driver.Manage().Window.Size = new System.Drawing.Size(948, 1003);
-    driver.TryToLoginWithClick("test@example.com", "1234asdf!!!!");
-
-    IWebElement booksIndexLink = driver.FindElement(By.LinkText("Books"));
-    WebDriverWait booksIndexLinkWait = new(driver, TimeSpan.FromSeconds(2));
-
-    booksIndexLinkWait.Until(d => booksIndexLink.Displayed);
-    booksIndexLink.Click();
-
-    IWebElement alphabeticalBooksIndexDiv = driver.FindElement(By.Id("alphabetical-books-index"));
-    int alphabeticaLinksCounter = alphabeticalBooksIndexDiv.FindElements(By.TagName("a")).Count;
-
-    IWebElement treeBooksIndexDiv = driver.FindElement(By.Id("tree-books-index"));
-    int treeLinksCounter = treeBooksIndexDiv.FindElements(By.TagName("a")).Count;
-    Assert.Multiple(() =>
+    private void DoTest()
     {
-      Assert.That(treeLinksCounter, Is.Not.Zero);
-      Assert.That(alphabeticaLinksCounter, Is.EqualTo(treeLinksCounter));
-    });
-  }
+        driver.Navigate().GoToUrl("http://localhost:3000/");
+        driver.TryToLoginWithClick("test@example.com", "1234asdf!!!!");
+        GoToBooks(driver);
+        CheckTwoBookIndexesEqualCounts(driver);
+    }
+
+    private static void GoToBooks(IWebDriver driver)
+    {
+        IWebElement booksIndexLink = driver.FindElement(By.LinkText("Books"));
+        WebDriverWait booksIndexLinkWait = new(driver, TimeSpan.FromSeconds(2));
+
+        booksIndexLinkWait.Until(d => booksIndexLink.Displayed);
+        booksIndexLink.Click();
+    }
+
+    private static void CheckTwoBookIndexesEqualCounts(IWebDriver driver)
+    {
+        IWebElement alphabeticalBooksIndexDiv = driver.FindElement(By.Id("alphabetical-books-index"));
+        int alphabeticaLinksCounter = alphabeticalBooksIndexDiv.FindElements(By.TagName("a")).Count;
+
+        IWebElement treeBooksIndexDiv = driver.FindElement(By.Id("tree-books-index"));
+        int treeLinksCounter = treeBooksIndexDiv.FindElements(By.TagName("a")).Count;
+        Assert.Multiple(() =>
+        {
+            Assert.That(treeLinksCounter, Is.Not.Zero);
+            Assert.That(alphabeticaLinksCounter, Is.EqualTo(treeLinksCounter));
+        });
+    }
+
+    [Test]
+    public void LargeScreenTest()
+    {
+        driver.Manage().Window.Size = new System.Drawing.Size(1600, 760);
+        DoTest();
+    }
+
+    [Test]
+    public void MediumScreenTest()
+    {
+        driver.Manage().Window.Size = new System.Drawing.Size(948, 1003);
+        DoTest();
+    }
+
+    [Test]
+    public void SmallScreenTest()
+    {
+        driver.Manage().Window.Size = new System.Drawing.Size(350, 600);
+        DoTest();
+    }
 }
