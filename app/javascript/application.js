030533c88d86625a60021a92b90a5afa74653820
Kyle Rego <regoky@outlook.com>
2024-11-09 14:58:49 -0500

    Change way Trix is configured

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,42 @@
 // is licensed under the GNU Affero General Public License, version 3
 // Copyright (C) 2023 Kyle Rego
 
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Configure your import map in config/importmap.rb.
+// Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
 
-import Trix from "trix"
+import Trix from "trix";
+
+Trix.config.blockAttributes.heading2 = {
+  tagName: "h2",
+  terminal: true,
+  breakOnReturn: true,
+  group: false,
+};
+Trix.config.blockAttributes.heading3 = {
+  tagName: "h3",
+  terminal: true,
+  breakOnReturn: true,
+  group: false,
+};
+Trix.config.blockAttributes.heading4 = {
+  tagName: "h4",
+  terminal: true,
+  breakOnReturn: true,
+  group: false,
+};
+Trix.config.blockAttributes.heading5 = {
+  tagName: "h5",
+  terminal: true,
+  breakOnReturn: true,
+  group: false,
+};
+Trix.config.blockAttributes.heading6 = {
+  tagName: "h6",
+  terminal: true,
+  breakOnReturn: true,
+  group: false,
+};
