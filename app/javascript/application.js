58b47e8ca1ba28da93873b5343665a852d0980e0
Kyle Rego <regoky@outlook.com>
2024-12-18 13:06:12 -0800

    Add in bootstrap and work on refactoring nav

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,10 +4,11 @@
 
 // Configure your import map in config/importmap.rb.
 // Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "trix"
-import "@rails/actiontext"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "trix";
+import "@rails/actiontext";
+import "bootstrap";
 
 import Trix from "trix";
 
