0ae2713086c8fd1205e28ad59dc07091af6f3aa0
Kyle Rego <regoky@outlook.com>
2025-01-03 14:56:34 -0800

    Continue working in bootstrap, work on books tree stimulus design

--- app/javascript/controllers/book_link_tree_node_controller.js
+++ app/javascript/controllers/book_link_tree_node_controller.js
@@ -7,23 +7,17 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "childrenBookLinkNodes", "expandButton", "unexpandButton" ];
 
-  initialize() {
-    this.showHiddenChildBookLinkNodes = this.showHiddenChildBookLinkNodes.bind(this);
-    this.hideShownChildBookLinkNodes = this.hideShownChildBookLinkNodes.bind(this);
-  }
-
-  connect() {
-    this.expandButtonTarget.addEventListener("click", this.showHiddenChildBookLinkNodes);
-    this.unexpandButtonTarget.addEventListener("click", this.hideShownChildBookLinkNodes);
-  }
-
-  showHiddenChildBookLinkNodes() {
+  showHiddenChildBookLinkNodes(event) {
+    event.preventDefault();
+    event.stopPropagation();
     this.childrenBookLinkNodesTarget.hidden = false;
     this.expandButtonTarget.hidden = true;
     this.unexpandButtonTarget.hidden = false;
   }
 
-  hideShownChildBookLinkNodes() {
+  hideShownChildBookLinkNodes(event) {
+    event.preventDefault();
+    event.stopPropagation();
     this.childrenBookLinkNodesTarget.hidden = true;
     this.expandButtonTarget.hidden = false;
     this.unexpandButtonTarget.hidden = true;
